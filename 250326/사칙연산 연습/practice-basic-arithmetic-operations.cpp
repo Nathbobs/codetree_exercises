a529c5d2f4a1cc4574f6d1ba36de076ed1e6d56a
Nathaniel ABEGUNDE <aanathaniel@gmail.com>
2025-03-26 21:07:37 +0900

    Title: 사칙연산 연습, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250326/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 30;
+    int c = a + b;
+
+    int quot = c/8;
+    int rem = c%8;
+
+    cout << quot;
+    cout << "\n";
+    cout << rem;
+    return 0;
+}
