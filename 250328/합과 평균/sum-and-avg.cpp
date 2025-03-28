d14c580ab27d518c1e2af20449c69aabc24536d6
Nathaniel ABEGUNDE <aanathaniel@gmail.com>
2025-03-28 17:52:14 +0900

    Title: 합과 평균, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250328/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    int sum = a+b;
+    float avg = (double)sum/ 2;
+    cout << sum << " " << avg;
+    return 0;
+}
