201e20e4a83c522ee19c36363561d3ad43d5adaa
Nathaniel ABEGUNDE <aanathaniel@gmail.com>
2025-06-09 11:15:51 +0900

    Title: 두 수의 거듭제곱, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250609/두 수의 거듭제곱/two-numbers-of-squares.cpp
+++ 250609/두 수의 거듭제곱/two-numbers-of-squares.cpp
@@ -2,14 +2,22 @@
 
 using namespace std;
 
-int square (int a, int b){
-    while( 1 <= a && b <= 10){
-        for (int i = 0; i < b; i++){
-            a *= a * b[i];
+// int power (int a, int b){
+//     while( 1 <= a && b <= 10){
+//         for (int i = 0; i < b; i++){
+//             a *= a * b[i];
 
-            return a;
-        }
+//             return a;
+//         }
+//     }
+// }
+
+int power (int a, int b){
+    int result = 1;
+    for (int i = 0; i < b; i++){
+        result *= a;
     }
+    return result;
 }
 
 
@@ -18,7 +26,9 @@ int a, b;
 int main() {
     cin >> a >> b;
 
-    cout << square(a,b);
+    if ( a >= 1 && b <=10){
+        cout << power(a,b);
+    }
 
     // Please write your code here.
 
